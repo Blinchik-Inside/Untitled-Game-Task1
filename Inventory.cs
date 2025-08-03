c8a5a6c159b9da2b74b081a69094d0864b414a15
Blinchik-Inside <anna.kediarova@gmail.com>
2025-08-03 11:10:08 -0400

    Work started

---
+++ Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Inventory {
+	private int size;
+	private Item items[];
+
+	public Inventory() {
+		size = 10;
+		items = new Item[size];
+	}
+
+	public Inventory(int size) {
+		this.size = size;
+        items = new Item[size];
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Item {
+	private int ID;
+	private string name;
+	private int count;
+	private string description;
+	private int healingFactor;
+
+	public Item(int id, string name) {
+        this.ID = id;
+        this.name = name;
+		count = 1;
+		description = "";
+		healingFactor = 0;
+	}
+
+
+}
